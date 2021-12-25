8c1603342dc6ea322e33d3615f758375a4cf9c70
emya <e.ayada810004@gmail.com>
2021-12-25 01:37:17 -0800

    change logo and background color

--- js/packages/web/src/components/AppBar/index.tsx
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -51,7 +51,7 @@ export const MetaplexMenu = () => {
     return (
       <>
         <Modal
-          title={<img src={'/metaplex-logo.svg'} />}
+          title={<img src={'/test_meta_tanuki.png'} style={{ width: '200px' }}/>}
           visible={isModalVisible}
           footer={null}
           className={'modal-box'}
@@ -110,7 +110,7 @@ export const MetaplexMenu = () => {
 export const LogoLink = () => {
   return (
     <Link to={`/`}>
-      <img src={'/metaplex-logo.svg'} />
+      <img src={'/test_meta_tanuki.png'} style={{ width: '200px' }}/>
     </Link>
   );
 };
